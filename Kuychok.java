28ff812987c8083b959d8248f2dd37019a3d6ca8
Tafire13 <67011212063@msu.ac.th>
2025-07-24 20:03:26 +0700

    update

--- Kuychok.java
+++ Kuychok.java
@@ -1,4 +1,6 @@
 
+import java.awt.Button;
+import java.awt.GridLayout;
 import javax.swing.JFrame;
 
 public class Kuychok extends JFrame {
@@ -11,7 +13,8 @@ public class Kuychok extends JFrame {
         setSize(300, 300);
         setTitle("Kuy Chok");
         setLocationRelativeTo(null);
-        setLayout(null);
+        setLayout(new GridLayout(5, 4));
+        createButton();
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setVisible(true);
     }
@@ -19,5 +22,13 @@ public class Kuychok extends JFrame {
     public static void main(String[] args) {
         new Kuychok();
 
+    }
+
+    public void createButton() {
+        Button[] buttons = new Button[20];
+        for (int i = 0; i < buttons.length; i++) {
+            buttons[i] = new Button("" + i);
+            add(buttons[i]);
+        }
     }
 }
